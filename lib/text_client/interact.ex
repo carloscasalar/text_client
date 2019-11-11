68c612e951fb9f9229dc73473c31a28b1d58bf12
Carlos Castillo <castillo.st@gmail.com>
2019-11-11 21:21:01 +0100

    Create modules to manage interaction and game state

---
+++ lib/text_client/interact.ex
@@ -0,0 +1,22 @@
+defmodule TextClient.Interact do
+
+  alias TextClient.State
+
+  def start() do
+    Hangman.new_game()
+    |> setup_state()
+    |> IO.inspect
+  end
+
+  def play(state) do
+
+  end
+
+  defp setup_state(game) do
+    %State{
+      game_service: game,
+      tally:        Hangman.tally(game),
+    }
+  end
+
+end
