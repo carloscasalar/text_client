e7c89706d6bc3cdda673bc63850a257c6673db24
Carlos Castillo <castillo.st@gmail.com>
2019-12-24 14:26:48 +0100

    Make prompter ask for letters in a loop

--- lib/text_client/prompter.ex
+++ lib/text_client/prompter.ex
@@ -1,7 +1,31 @@
 defmodule TextClient.Prompter do
-  def accept_move(game) do
-    input = IO.gets("Your guess:")
-    IO.inspect(input)
+  alias TextClient.State
+
+  def accept_move(game = %State{}) do
+    IO.gets("Your guess: ")
+    |> check_input(game)
+  end
+
+  defp check_input({:error, reason}, _game) do
+    IO.puts("Game ended: #{reason}")
     exit(:normal)
+  end
+
+  defp check_input(:eof, _game) do
+    IO.puts("Game ended: looks like you gave up")
+    exit(:normal)
+  end
+
+  defp check_input(input, game = %State{}) do
+    input = String.trim(input)
+
+    cond do
+      input =~ ~r/\A[a-z]\z/ ->
+        Map.put(game, :guess, input)
+
+      true ->
+        IO.puts("Please, enter a single lowercase letter")
+        accept_move(game)
+    end
   end
 end
--- lib/text_client/summary.ex
+++ lib/text_client/summary.ex
@@ -1,9 +1,11 @@
 defmodule TextClient.Summary do
-  def display(%{tally: tally}) do
+  def display(game = %{tally: tally}) do
     IO.puts([
       "\n",
       "word so far: #{Enum.join(tally.letters, " ")}\n",
-      "Guesses left: #{tally.turns_left}"
+      "Guesses left: #{tally.turns_left}\n"
     ])
+
+    game
   end
 end
